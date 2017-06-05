bfa791c5e76b99a10b1241f23df5956d58e3d87c
Artem Kondratev <konapt@gmail.com>
2017-06-06 01:29:11 +0300

    dealing cards and some formatting

--- lib/deck.ex
+++ lib/deck.ex
@@ -5,26 +5,26 @@ defmodule Deck do
 
   def cards do
     %{
-    	"pine"          => ["plain1", "plain2", "ribbon", "bright"],
-    	"plum"          => ["plain1", "plain2", "ribbon", "animal"],
-    	"sakura"        => ["plain1", "plain2", "ribbon", "bright"],
-    	"wisteria"      => ["plain1", "plain2", "ribbon", "animal"],
-    	"iris"          => ["plain1", "plain2", "ribbon", "animal"],
-    	"peony"         => ["plain1", "plain2", "ribbon", "animal"],
-    	"clover"        => ["plain1", "plain2", "ribbon", "animal"],
-    	"pampas"        => ["plain1", "plain2", "animal", "bright"],
-    	"chrysanthemum" => ["plain1", "plain2", "ribbon", "animal"],
-    	"maple"         => ["plain1", "plain2", "ribbon", "animal"],
-    	"willow"        => ["plain1", "ribbon", "animal", "bright"],
-    	"paulownia"     => ["plain1", "plain2", "plain3", "bright"],
+      "pine"          => ["plain1", "plain2", "ribbon", "bright"],
+      "plum"          => ["plain1", "plain2", "ribbon", "animal"],
+      "sakura"        => ["plain1", "plain2", "ribbon", "bright"],
+      "wisteria"      => ["plain1", "plain2", "ribbon", "animal"],
+      "iris"          => ["plain1", "plain2", "ribbon", "animal"],
+      "peony"         => ["plain1", "plain2", "ribbon", "animal"],
+      "clover"        => ["plain1", "plain2", "ribbon", "animal"],
+      "pampas"        => ["plain1", "plain2", "animal", "bright"],
+      "chrysanthemum" => ["plain1", "plain2", "ribbon", "animal"],
+      "maple"         => ["plain1", "plain2", "ribbon", "animal"],
+      "willow"        => ["plain1", "ribbon", "animal", "bright"],
+      "paulownia"     => ["plain1", "plain2", "plain3", "bright"],
     }
   end
 
   def create(cards) do
     Enum.flat_map cards, fn({suit, values}) ->
-        for value <- values do
-            %{suit: suit, value: value}
-        end
+      for value <- values do
+        %{suit: suit, value: value}
+      end
     end
   end
 
@@ -34,8 +34,18 @@ defmodule Deck do
 
   def serve_deck do
     deck = cards()
-    |> create
-    |> shuffle
+      |> create
+      |> shuffle
+  end
+  
+  def open_card(deck) do
+    [card | _tail] = deck
+    card
+  end
+
+  def deal(deck) do
+    [card | tail] = deck
+    {card, tail}
   end
 
   
